6f7939fc51e8c8f32a3a0c54da115bede4f09458
yunhozz <qkrdbsgh1121@naver.com>
2022-04-18 16:55:57 +0900

    "first commit"

--- src/main/java/study/querydsl/entity/Hello.java
+++ src/main/java/study/querydsl/entity/Hello.java
@@ -7,6 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/*
+Gradle -> Tasks -> other -> compileQuerydsl 실행
+generated/querydsl/study/querydsl/entity/QHello.java 생성
+ */
 @Entity
 @Getter @Setter
 public class Hello {
