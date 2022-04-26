46cca921112b2d518a28207ff018f7f6047818c4
yunhozz <qkrdbsgh1121@naver.com>
2022-04-26 15:59:17 +0900

    first commit

--- src/test/java/study/querydsl/repo/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repo/MemberRepositoryTest.java
@@ -16,7 +16,6 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
